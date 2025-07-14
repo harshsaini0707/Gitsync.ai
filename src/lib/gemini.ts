ff23eeec43611252e7b9e2bcee933189d41b5561
harshsaini0707 <harshsaini0172@gmail.com>
2025-07-14 19:04:33 +0530

    key issue

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+const genAI = new GoogleGenerativeAI(process.env.Gemini_API_KEY!);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 import { Document } from "@langchain/core/documents";
 
